b0c0fe03889a41c67758b07acc375d6188193f6e
CalinNavadaru <66365479+CalinNavadaru@users.noreply.github.com>
2021-01-28 11:49:15 +0200

    vgbhn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python test.py
+web: gunicorn gettingstarted.wsgi --log-file -
--- backend.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-Client = pymongo.MongoClient("mongodb+srv://Calin:GUjTnNlQ56yD8ntL@matearenacluster.x6qt4.mongodb.net/test")
-
-DB = Client['Probleme']
-
-print(DB.list_collection_names())
